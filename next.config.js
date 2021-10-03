85729b933deff24cebbda33c062f32f392df4ed4
RIO <riohifi01@gmail.com>
2021-10-04 01:20:52 +0530

    modify asset

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
-   assetPrefix: isProd ? 'http://app.webmydig.com' : ''
+   assetPrefix: 'http://app.webmydig.com'
 }
