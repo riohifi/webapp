0282cfb522da2a8be6931751f5500afc39e4df17
RIO <riohifi01@gmail.com>
2021-10-15 03:59:52 +0530

    add dev

---
+++ pages/Dev.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Dev() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Dev Us</h1>
+        <span>ok too </span>
+      </div>
+    </div>
+  )
+}
