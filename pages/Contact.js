12cf39950ae214d3a824aecb32c75099ccbde347
RIO <riohifi01@gmail.com>
2021-10-15 14:10:05 +0530

    add moment

--- pages/Contact.js
+++ pages/Contact.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import moment from 'moment';
+
 export default function Contact() {
   return (
     <div className={styles.container}>
       <div>
         <h1>Contact Us</h1>
-        <span>Hii Help Me Rio </span>
+        <span>Hii Help Me Rio {moment('12/12/2012').format('DD-MM-YY')} </span>
       </div>
     </div>
   )
