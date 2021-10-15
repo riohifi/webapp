a2df7c3336ee8ec6cbbf0b0a944457224d3cfbce
RIO <riohifi01@gmail.com>
2021-10-15 13:50:08 +0530

    mod dev v3

--- pages/Contact.js
+++ pages/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
     <div className={styles.container}>
       <div>
         <h1>Contact Us</h1>
-        <span>ok too </span>
+        <span>Help Me </span>
       </div>
     </div>
   )
